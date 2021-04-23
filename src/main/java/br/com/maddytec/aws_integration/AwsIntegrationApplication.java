68d6bb1234761a56f37de9bf4c11535e4b05374f
maddytec <maddytec@gmail.com>
2021-04-23 08:14:41 -0300

    first commit

---
+++ src/main/java/br/com/maddytec/aws_integration/AwsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.maddytec.aws_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsIntegrationApplication.class, args);
+	}
+
+}
