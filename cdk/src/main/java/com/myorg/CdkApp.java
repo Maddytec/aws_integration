da2250ec931708e4e2480afbc34e7cd05b408e32
maddytec <maddytec@gmail.com>
2021-05-01 09:13:18 -0300

    Adicionado CDK, criação de VPC na AWS

---
+++ cdk/src/main/java/com/myorg/CdkApp.java
@@ -0,0 +1,16 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
+
+import java.util.Arrays;
+
+public class CdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new VpcStack(app, "Vpc");
+        app.synth();
+    }
+}
