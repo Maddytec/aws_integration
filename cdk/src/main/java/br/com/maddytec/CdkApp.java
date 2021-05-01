c77aca7187a55cec1f6085509307571685a6518e
maddytec <maddytec@gmail.com>
2021-05-01 09:24:22 -0300

    Alterado o packge do projeto

--- cdk/src/main/java/br/com/maddytec/CdkApp.java
+++ cdk/src/main/java/br/com/maddytec/CdkApp.java
@@ -1,10 +1,6 @@
-package com.myorg;
+package br.com.maddytec;
 
 import software.amazon.awscdk.core.App;
-import software.amazon.awscdk.core.Environment;
-import software.amazon.awscdk.core.StackProps;
-
-import java.util.Arrays;
 
 public class CdkApp {
     public static void main(final String[] args) {
--- cdk/src/main/java/br/com/maddytec/VpcStack.java
+++ cdk/src/main/java/br/com/maddytec/VpcStack.java
@@ -1,4 +1,4 @@
-package com.myorg;
+package br.com.maddytec;
 
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
