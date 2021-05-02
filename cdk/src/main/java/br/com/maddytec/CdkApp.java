42a47871599e2eb22b54a0fa76ef389bb76b0105
maddytec <maddytec@gmail.com>
2021-05-02 06:44:49 -0300

    Criação do Cluster na AWS com CDK

--- cdk/src/main/java/br/com/maddytec/CdkApp.java
+++ cdk/src/main/java/br/com/maddytec/CdkApp.java
@@ -1,12 +1,16 @@
 package br.com.maddytec;
 
 import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.services.ecs.Cluster;
 
 public class CdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new VpcStack(app, "Vpc");
+        VpcStack vpc = new VpcStack(app, "Vpc");
+        ClusterStack cluster = new ClusterStack(app, "Cluster", vpc.getVpc());
+        cluster.addDependency(vpc);
+
         app.synth();
     }
 }
--- cdk/src/main/java/br/com/maddytec/VpcStack.java
+++ cdk/src/main/java/br/com/maddytec/VpcStack.java
@@ -1,11 +1,16 @@
 package br.com.maddytec;
 
+import lombok.Getter;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
 
 public class VpcStack extends Stack {
+
+    @Getter
+    private Vpc vpc;
+
     public VpcStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -13,7 +18,7 @@ public class VpcStack extends Stack {
     public VpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Vpc.Builder.create(this, "VpcMaddytec01")
+       vpc = Vpc.Builder.create(this, "VpcMaddytec01")
                 .maxAzs(3)
                 .build();
     }
